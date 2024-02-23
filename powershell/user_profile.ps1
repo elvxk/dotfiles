8ae340f4dfa78d15a0554ebadeb0139a78f57ef1
elvxk <pradiptaramadan9c@gmail.com>
2024-02-23 08:46:22 +0700

    🎣 Update pwsh profile

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -1,11 +1,10 @@
-# eval "$(oh-my-posh init bash --config D:/laragon/www/kirby-omp/kirby.omp.json)"
+# Delete watermark pwsh
+Clear-Host
 
-# function Get-ScriptDirectory { Split-Path $MyInvocation.ScriptName}
-# $PROMPT_CONFIG = Join-Path (Get-ScriptDirectory) 'kirby.omp.json'
-
-cls
-
+# Import terminal icons / https://github.com/devblackops/Terminal-Icons
 Import-Module Terminal-Icons
+
+# init oh my posh / https://ohmyposh.dev/docs/themes
 oh-my-posh --init --shell pwsh --config  $HOME/.config/powershell/kirby.omp.json | Invoke-Expression
 
 # Set Alias
@@ -14,44 +13,106 @@ Set-Alias g git
 Set-Alias n npm
 
 # Refresh profile pwsh / reload pwsh
-function sc {Invoke-Command {& "pwsh.exe" -nologo} -NoNewScope}
+function sc
+{
+  Invoke-Command {& "pwsh.exe" -nologo} -NoNewScope
+}
 
 # Goto location
-function cdw { cd D:/laragon/www }
-function cdv { cd $HOME/AppData/Local/nvim }
-function kirby { cd D:/laragon/www/kirby-omp }
+function cdw
+{
+  Set-Location D:/laragon/www
+}
+function cdv
+{
+  Set-Location $HOME/AppData/Local/nvim
+}
+function kirby
+{
+  Set-Location D:/laragon/www/kirby-omp
+}
+function cdc
+{
+  Set-Location $HOME/.config/
+}
 
 # npm vite
-function nrd { npm run dev -- $args }
-function nrb { npm run build }
-function nrp { npm run preview }
+function nrd
+{
+  npm run dev -- $args
+}
+function nrb
+{
+  npm run build
+}
+function nrp
+{
+  npm run preview
+}
 
 # Create react app
-function cra { npm create vite@latest $args }
-function react { git clone https://github.com/elvxk/react-tailwind-starter.git $args}
+function cra
+{
+  npm create vite@latest $args
+}
+function react
+{
+  git clone https://github.com/elvxk/react-tailwind-starter.git $args
+}
 
 # Edit file in nvim
-function vp { nvim $HOME/.config/powershell/user_profile.ps1 }
+function vp
+{
+  nvim $HOME/.config/powershell/user_profile.ps1
+}
 
 # Git
-function gcl { git clone $args}
-function gf { git fetch }
-function gb { git branch $args}
-function gs { git status }
-function ga { git add . }
-function gmit {
-  do{
+function gcl
+{
+  git clone $args
+}
+function gf
+{
+  git fetch
+}
+function gb
+{
+  git branch $args
+}
+function gs
+{
+  git status
+}
+function ga
+{
+  git add .
+}
+function gmit
+{
+  do
+  {
     $commit = Read-Host -Prompt "🍧 Enter commit "
     $commit_check = $commit -replace '\s',''
-    if($commit_check){
-      Write-Host "" 
+    if($commit_check)
+    {
+      Write-Host ""
       git commit -m $commit
-    }else{
+    } else
+    {
       Write-Host " - Please input commit" -ForegroundColor Red
     }
   }while(!($commit_check))
 }
-function gcp { git commit -m "📦 Paket..." }
-function gpull { git pull }
-function gpush { git push -u origin $args }
+function gcp
+{
+  git commit -m "📦 Paket..."
+}
+function gpull
+{
+  git pull
+}
+function gpush
+{
+  git push -u origin $args
+}
 
